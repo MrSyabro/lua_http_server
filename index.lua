6eda8803e433ede9af7faf60fb8a8100d2cbe325
MrSyabro <syabro1998@gmail.com>
2023-12-18 01:46:09 +0200

    микроправки

--- index.lua
+++ index.lua
@@ -3,11 +3,11 @@ local page = dofile "page.lua"
 echo(page("Home", ([[
         <div class="container my-5">
             <h1 class="fw-light text-center">Скрипт %s </h1><br>
+            <hr>
             <div class="row">
                 <div class="col-lg-6 mx-auto">
                     <p class="lead">Эта страница динамическая и уже куда более интересна.</p>
-                    <p class="lead">В последнем обновлении была добавлена микро библиотека для генерации HTML функциями Lua. Скрипт <a href="forms.lua">forms</a> демонстрирует ее работу.</p>
-                    <hr>
+                    <p class="lead">В последнем обновлении была добавлена микро библиотека для генерации HTML функциями Lua.</p>
                     <p class="lead text-muted">$NAME = %s</p>
                     <p class="lead text-muted">Дата на сервере: %s</p>
                 </div>
