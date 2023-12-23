12fcafdd37143a87b8a7b8ffcfc22aef0cdb85be
MrSyabro <syabro1998@gmail.com>
2023-12-24 00:36:20 +0200

    небольшие правки верски страниц

--- head.lua
+++ head.lua
@@ -12,6 +12,12 @@ return function(page_name)
             integrity = "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
             crossorigin = "anonymous"
         },
+        h.script({src="https://unpkg.com/htmx.org@1.9.9"}, ""),
+        h.script({
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
+            crossorigin="anonymous"}, ""
+        ),
         h.title(nil, page_name)
     )
 end
--- page.lua
+++ page.lua
@@ -9,12 +9,6 @@ return function(name, body)
     h.html({lang = "ru"},
         head(name),
         h.body(nil,
-            h.script({src="https://unpkg.com/htmx.org@1.9.9"}, ""),
-            h.script({
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
-                integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
-                crossorigin="anonymous"}, ""
-            ),
             dofile "navbar.lua",
             body
         )
