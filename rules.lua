0567ccce2a8a6c20f3cd83a173895bc55e97556f
MrSyabro <syabro1998@gmail.com>
2024-02-14 22:54:10 +0200

    Update rules.lua
    fix: обновление правил

--- rules.lua
+++ rules.lua
@@ -1,5 +1,5 @@
-local function index(request)
-	request.url.path = "/index.lua"
+local function index(s)
+	s.request.url.path = "/index.lua"
 end
 
 return {
@@ -7,15 +7,15 @@ return {
 	{regex = "pem", func = index},
 	{regex = "index$", func = index},
 
-	{regex = "files", func = function(request) request.url.path = "/files.lua" end},
-	{regex = "dump%-headers", func = function(request) request.url.path = "/dump-headers.lua" end},
-	{regex = "getfile", func = function(request) request.url.path = "/getfile.lua" end},
+	{regex = "files", func = function(s) s.url.path = "/files.lua" end},
+	{regex = "dump%-headers", func = function(s) s.request.url.path = "/dump-headers.lua" end},
+	{regex = "getfile", func = function(s) s.request.url.path = "/getfile.lua" end},
 
-	{regex = ".png", func = function(request, response) response.headers["Content-Type"] = "image/png" end},
-	{regex = ".ico", func = function(request, response) response.headers["Content-Type"] = "image/icon" end},
-	{regex = ".json", func = function(request, response) response.headers["Content-Type"] = "application/json" end},
-	{regex = ".svg", func = function(request, response) response.headers["Content-Type"] = "image/svg+xml" end},
-	{regex = ".css", func = function(request, response) response.headers["Content-Type"] = "image/css" end},
+	{regex = ".png", func = function(s) s.response.headers["Content-Type"] = "image/png" end},
+	{regex = ".ico", func = function(s) s.response.headers["Content-Type"] = "image/icon" end},
+	{regex = ".json", func = function(s) s.response.headers["Content-Type"] = "application/json" end},
+	{regex = ".svg", func = function(s) s.response.headers["Content-Type"] = "image/svg+xml" end},
+	{regex = ".css", func = function(s) s.response.headers["Content-Type"] = "image/css" end},
 
 	{regex = "/$", func = index},
 }
